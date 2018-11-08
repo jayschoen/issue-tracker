08556d95962c0c68ced46423ee3ee4b1390212be
Jay Schoen <jay.r.schoen@gmail.com>
2018-11-08 12:54:15 -0500

    #4 good old bootstrap for grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { IssueBoxComponent } from './issue-box/issue-box.component';
+import { IssueRowComponent } from './issue-row/issue-row.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MainDashboardComponent
+    MainDashboardComponent,
+    IssueBoxComponent,
+    IssueRowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
