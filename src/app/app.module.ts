c87530c661937ede26f250849febcaeff9fee584
Jay Schoen <jay.r.schoen@gmail.com>
2018-11-13 11:09:02 -0500

    '#7 remove issue-row component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 import { IssueBoxComponent } from './issue-box/issue-box.component';
-import { IssueRowComponent } from './issue-row/issue-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainDashboardComponent,
-    IssueBoxComponent,
-    IssueRowComponent
+    IssueBoxComponent
   ],
   imports: [
     BrowserModule
