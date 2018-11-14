e3176c30bc90f837d65c9d4248ba4122dd71e200
Jay Schoen <jay.r.schoen@gmail.com>
2018-11-14 12:56:32 -0500

    #2 basics... no saved token; need to flesh out a bit more

--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IssuesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getIssues() {
+    return this.httpClient.get("https://api.github.com/repos/jayschoen/issue-tracker/issues?state=all&sort=created&direction=asc&access_token=???");
+  }
 }
