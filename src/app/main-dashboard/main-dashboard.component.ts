12cc1c0e09f2c04030eabc46f369bd14ee445fde
Jay Schoen <jay.r.schoen@gmail.com>
2019-04-24 12:42:03 -0400

    #20 darken background and modify responsiveness

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -15,15 +15,14 @@ export class MainDashboardComponent implements OnInit {
 
   interval: any;
 
-  breakpoint = 4;
+  issue_width = window.innerWidth <= 1250 ? 3 : 4;
+  issue_height = window.innerWidth <= 1250 ? '4:1' : '3:1';
 
   constructor(
     private issuesService: IssuesService
   ) {}
 
   ngOnInit() {
-
-    this.breakpoint = (window.innerWidth <= 1250) ? 3 : 4;
 
     this.issuesService.data$.subscribe(
       data => this.processed_repos = data,
@@ -36,7 +35,43 @@ export class MainDashboardComponent implements OnInit {
     }, 300000);
   }
 
-  checkWindowSize(event) {
-    this.breakpoint = (event.target.innerWidth <= 1250) ? 3 : 4;
+  checkWindowSize(event?) {
+    let window_width = window.innerWidth;
+    const issue_size = {};
+
+    if (event) {
+      window_width = event.target.innerWidth;
+      issue_size['width'] = (window_width <= 1250) ? 3 : 4;
+      issue_size['height'] = (window_width <= 1250) ? '4:1' : '3:1';
+    }
+
+    this.issue_width = issue_size['width'];
+    this.issue_height = issue_size['height'];
+
+    return issue_size;
+  }
+
+  calculateSize(issue_count, type) {
+    const size = {};
+
+    if (issue_count >= 4) {
+      size['width'] = this.issue_width;
+      size['height'] = this.issue_height; // '3:1';
+    } else if (issue_count === 3) {
+      size['width'] = 3;
+      size['height'] = '4:1';
+    } else if (issue_count === 2) {
+      size['width'] = 2;
+      size['height'] = '6:1';
+    } else {
+      size['width'] = 1;
+      size['height'] = '15:1';
+    }
+
+    if (type === 'width') {
+      return size['width'];
+    } else {
+      return size['height'];
+    }
   }
 }
